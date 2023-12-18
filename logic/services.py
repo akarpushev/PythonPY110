24c794a40dc96286ce9b4433da3095751181207e
Алексей <akarpushev@yandex.ru>
2023-12-18 04:00:50 +0300

    18.12.2023 лаб.3

--- logic/services.py
+++ logic/services.py
@@ -15,16 +15,15 @@ def filtering_category(database: dict,
     то возвращается пустой список
     """
     if category_key is not None:
-        result = []
-        for i in range(len(DATABASE)):
-            result = result.append(DATABASE['category'])
-                  #  TODO При помощи фильтрации в list comprehension профильтруйте товары по категории. Или можете использовать
-        # обычный цикл или функцию filter
-    else:
-        result = list(DATABASE) #  TODO Трансформируйте database в список словарей
-    if ordering_key is not None:
-        ... #  TODO Проведите сортировку result по ordering_key и параметру reverse
-    return result
+        if category_key is not None:
+            data = list(DATABASE.values())
+            result = [dict for dict in data if dict['category'] == category_key]
+
+        else:
+            result = list(database) #  TODO Трансформируйте database в список словарей
+        if ordering_key is not None:
+            result = sorted(result, key=lambda x: x[ordering_key], reverse=reverse)
+        return result
 
 
 if __name__ == "__main__":
--- store/views.py
+++ store/views.py
@@ -2,51 +2,35 @@ from django.shortcuts import render
 
 from django.http import JsonResponse, HttpResponse, HttpResponseNotFound
 from .models import DATABASE
-#from logic.services import filtering_category
+from logic.services import filtering_category
 
 def products_view(request):
     if request.method == "GET":
         if id_product := request.GET.get("id"):
             if data := DATABASE.get(id_product):
                 return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+            return HttpResponseNotFound("Данного продукта нет в базе данных")
         else:
             data = DATABASE
             return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
-        return HttpResponseNotFound("Данного продукта нет в базе данных")
+        # return HttpResponseNotFound("Данного продукта нет в базе данных")
 
-#        id = request.GET.get('id')
-#        if id:
-#           data = DATABASE[id]
-#       else:
-#            data = DATABASE
+        # Обработка фильтрации из параметров запроса
+    if category_key := request.GET.get("category"):  # Считали 'category'
+        if ordering_key := request.GET.get("ordering"):  # Если в параметрах есть 'ordering'
+            if request.GET.get("reverse") in ('true', 'True'):  # Если в параметрах есть 'ordering' и 'reverse'=True
+                data = filtering_category(DATABASE, category_key, ordering_key, reverse=True)
+                # TODO Провести фильтрацию с параметрами
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key, reverse=False)
+                # TODO Провести фильтрацию с параметрами
+        else:
+            data = filtering_category(DATABASE, category_key, ordering_key)
+            # TODO Провести фильтрацию с параметрами
+        # В этот раз добавляем параметр safe=False, для корректного отображения списка в JSON
+        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False,
+                                                                 'indent': 4})
 
-        #elif id != id in DATABASE:
-         #   return HttpResponseNotFound("Данного продукта нет в базе данных")
-        #else:
-         #   data = DATABASE
-#        return JsonResponse(data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
-        # Вернуть JsonResponse с объектом DATABASE и параметрами отступов и кодировок,
-        # как в приложении app_weather
-
-
-# 1-ый вариант
-# def products_page_view(request, page):
-#     if request.method == "GET":
-#         for data in DATABASE.values():
-#             if data['html'] == page:  # Если значение переданного параметра совпадает именем html файла
-#                 with open(f'store/products/{page}.html', encoding="utf-8") as f:
-#                     data = f.read()
-#                 return HttpResponse(data)
-#
-#         # TODO 1. Откройте файл open(f'store/products/{page}.html', encoding="utf-8") (Не забываем про контекстный менеджер with)
-#         # TODO 2. Прочитайте его содержимое
-#         # TODO 3. Верните HttpResponse c содержимым html файла
-#
-#         # Если за всё время поиска не было совпадений, то значит по данному имени нет соответствующей
-#         # страницы товара и можно вернуть ответ с ошибкой HttpResponse(status=404)
-#         return HttpResponse(status=404)
-
-# 2-ой вариант
 def products_page_view(request, page):
     if request.method == "GET":
         if isinstance(page, str):
@@ -62,13 +46,10 @@ def products_page_view(request, page):
                 with open(f'store/products/{data["html"]}.html', encoding="utf-8") as f:
                     data = f.read()
                 return HttpResponse(data)
-
                 # 1. Откройте файл open(f'store/products/{data["html"]}.html', encoding="utf-8") (Не забываем про контекстный менеджер with)
                 # 2. Прочитайте его содержимое
                 # 3. Верните HttpResponse c содержимым html файла
-
         return HttpResponse(status=404)
-
 
 
 def shop_view(request):
